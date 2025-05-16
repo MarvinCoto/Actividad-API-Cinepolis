2690b9f0521cf65dd407092d34a5da4f66909401
Marvin Coto <20230644@ricaldone.edu.sv>
2025-05-16 09:00:15 -0600

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+// Importo todo lo de la librería de Express
+import express from "express";
+import cookieParser from "cookie-parser";
+
+//Creo una constante que es igual a la librería que importé
+const app = express();
+
+//Que acepte datos en json
+app.use(express.json());
+
+//Que postman acepte guardar cookies
+app.use(cookieParser());
+
+//Definimos las rutas de las funciones que tendrá la página web
+
+
+//Exporto la constante para poder usar express en otros archivos
+export default app;
